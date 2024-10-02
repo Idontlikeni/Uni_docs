cb7ad4a8a9dab7c22c1d57b0ae61dbda433adbc1
Timur Tuktarov <tima.xxxyyy@mail.ru>
2024-10-02 12:22:50 +0300

    Architech PR_1 update

--- Achitech/misc.py
+++ Achitech/misc.py
@@ -1,2 +1,14 @@
-for i in range(32):
-    print(f"<a name=\"bit{i}\" val=\"{31 - i}\"/>")
+f = '0100 0111 1000 1110 1001 1100 0001 0110'.replace(' ', '')
+sdnf = ''
+sknf = ''
+for i in range(len(f)):
+    s = bin(i)[2:].rjust(5, '0')
+    if f[i] == '1':
+        sknf += '-'
+    else:
+        for j in range(5):
+            if s[j] == '0':
+                sdnf = "\\" + "bar"
+            else: sdnf += 'x_{' + str(j) + '}'
+        sdnf += '+'
+print(sdnf)
