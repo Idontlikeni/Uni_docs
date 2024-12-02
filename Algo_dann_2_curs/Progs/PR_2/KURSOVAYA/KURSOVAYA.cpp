26ed8b6aa2e768ceb6a2f5faab2321b49f7d5a8d
Timur Tuktarov <tima.xxxyyy@mail.ru>
2024-12-02 18:40:13 +0300

    changes on architech, algos kursov

--- Algo_dann_2_curs/Progs/PR_2/KURSOVAYA/KURSOVAYA.cpp
+++ Algo_dann_2_curs/Progs/PR_2/KURSOVAYA/KURSOVAYA.cpp
@@ -16,22 +16,14 @@ struct info {
         s2 += other.f + other.i + other.o;
         return (s1 < s2);
     }
-    
+
     string get_data() {
         return f + " " + i + " " + o + " | " + to_string(number) + " " + to_string(series);
     }
 };
 
-//bool operator<(const info& t1, const info& t2)
-//{
-//    string s1 = "", s2 = "";
-//    for (int i = 0; i < 3; i++)
-//    {
-//        s1 += t1.fio[i];
-//        s2 += t2.fio[i];
-//    }
-//    return (s1 < s2);
-//}
+ifstream fin;
+ofstream fout;
 
 void print_instructions() {
     cout << "1 - создать пустой словарь\n";
@@ -40,6 +32,8 @@ void print_instructions() {
     cout << "4 - найти элемент в словаре по ключу\n";
     cout << "5 - изменить значение элемента\n";
     cout << "6 - вывести словарь в порядке возрастания ключей\n";
+    cout << "7 - прочитать данные из текстового файла\n";
+    cout << "8 - записать словарь в файл\n";
     cout << "0 - записать словарь в файл и завершить работу программы\n";
 }
 
@@ -78,65 +72,78 @@ int read_instructions() {
     int n;
     while (1) {
         cin >> n;
-        if (n >= 0 && n <= 6)return n;
+        if (n >= 0 && n <= 8)return n;
+    }
+}
+
+void read_file(map<string, info>& mp) {
+    cout << "Введите путь к текстовому файлу:\n";
+    string path;
+    cin >> path;
+    fin.open(path);
+    if (!fin.is_open()) {
+        cout << "Не удалось открыть файл\n";
+        return;
+    }
+    while (!fin.eof()) {
+        string phone;
+        string fio[3];
+        int number, series;
+        fin >> phone >> fio[0] >> fio[1] >> fio[2] >> number >> series;
+        mp[phone] = info{ fio[0], fio[1], fio[2], number, series };
+    }
+}
+
+void write_file(map<string, info>& mp) {
+    fout.open("output.txt");
+    for (const auto& element : mp) {
+        fout << element.first << " " << element.second.f << " " << element.second.i << " " << element.second.o << " " << element.second.number << " " << element.second.series << "\n";
     }
 }
 
 int main()
 {
-    ifstream fin;
-    ofstream fout;
     setlocale(LC_ALL, "Ru");
-    cout << "Введите путь к текстовому файлу: ";
-    string path;
-    cin >> path;
-    fin.open(path);
     map<string, info> mp;
-    string f[] = {"a", "b", "c"};
-    if (!fin.is_open()) {
-        cout << "Не удалось открыть файл";
-    }
-    else {
-        while (!fin.eof()) {
-            string phone;
-            string fio[3];
-            int number, series;
-            fin >> phone >> fio[0] >> fio[1] >> fio[2] >> number >> series;
-            mp[phone] = info{ fio[0], fio[1], fio[2], number, series };
+    print_instructions();
+    bool f = true;
+    while (f) {
+        int instruction = read_instructions();
+        //cout << instruction << "\n";
+        switch (instruction)
+        {
+        case 0:
+            f = false;
+            break;
+        case 1:
+            mp.clear();
+            break;
+        case 2:
+            add_elem(mp);
+            break;
+        case 3:
+            delete_elem(mp);
+            break;
+        case 4:
+            find_elem(mp);
+            break;
+        case 5:
+            add_elem(mp);
+            break;
+        case 6:
+            print_map(mp);
+            break;
+        case 7:
+            read_file(mp);
+            break;
+        case 8:
+            write_file(mp);
+            break;
+        default:
+            break;
         }
+    }
         
-        print_instructions();
-        bool f = true;
-        while (f) {
-            int instruction = read_instructions();
-            switch (instruction)
-            {
-            case 0:
-                f = false;
-                break;
-            case 1:
-                mp.clear();
-                break;
-            case 2:
-                add_elem(mp);
-                break;
-            case 3:
-                delete_elem(mp);
-                break;
-            case 4:
-                find_elem(mp);
-            case 5:
-                add_elem(mp);
-            case 6:
-                print_map(mp);
-            default:
-                break;
-            }
-        }
-        fout.open("output.txt");
-        for (const auto& element : mp) {
-            fout << element.first << " " << element.second.f << " " << element.second.i << " " << element.second.o << " " << element.second.number << " " << element.second.series << "\n";
-        }
-    }
+    
     return 0;
 }
